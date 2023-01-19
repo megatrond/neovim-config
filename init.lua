0793bf9a0a30a7f01eed004cc01bbc2a2af0e14a
Trond Sørås <trond@dx.no>
2023-01-19 12:59:48 +0100

    map Alt+arrows to moving between windows

--- init.lua
+++ init.lua
@@ -11,8 +11,26 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+alt_send_esc = false
+
+-- use space as leader
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
+
+local n_mappings = {
+    -- move cursor between windows
+    ["<M-Up>"] = "<C-W>k",
+    ["<M-Down>"] = "<C-W>j",
+    ["<M-Left>"] = "<C-W>h",
+    ["<M-Right>"] = "<C-W>l",
+    -- center cursor after scroll is sweet
+    ["<C-d>"] = "<C-d>zz",
+    ["<C-u>"] = "<C-u>zz"
+}
+
+
+local utils = require("trond/utils")
+utils.map("n", n_mappings)
 
 require("lazy").setup("trond.plugins", opts)
 
